2f67366bd31c8bdc865bfe406912b175d75c146e
Walter Pagani <paganiwalter@gmail.com>
2023-06-17 18:07:13 -0300

    Adding functionality to the module (#9)

--- src/AoeLoot_SC.cpp
+++ src/AoeLoot_SC.cpp
@@ -15,7 +15,6 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "AoeLoot.h"
 #include "Log.h"
 #include "ScriptMgr.h"
 #include "Config.h"
@@ -23,19 +22,104 @@
 #include "Player.h"
 #include "ScriptedGossip.h"
 
-class AoeLoot_World : public WorldScript
+class AoeLoot_Player : public PlayerScript
 {
 public:
-    AoeLoot_World() : WorldScript("AoeLoot_World") { }
+    AoeLoot_Player() : PlayerScript("AoeLoot_Player") { }
 
-    void OnAfterConfigLoad(bool /*reload*/) override
+    void OnLogin(Player* player) override
     {
-        // Add conigs options configiration
+        if (sConfigMgr->GetOption<bool>("AOELoot.Enable", false))
+        {
+            ChatHandler(player->GetSession()).PSendSysMessage("This server is running the |cff4CFF00Loot aoe |r module.");
+        }
+    }
+
+    bool CanSendErrorArleadyLooted(Player* /*player*/) override
+    {
+        return true;
+    }
+
+    bool CanSendCreatureLoot(Creature* creature, Player* player) override
+    {
+        bool _Enable = sConfigMgr->GetOption<bool>("AOELoot.Enable", true);
+
+        if (player->GetGroup() || !creature || !_Enable)
+            return true;
+
+        float range = 30.0f;
+        uint32 gold = 0;
+
+        std::list<Creature*> creaturedie;
+        player->GetDeadCreatureListInGrid(creaturedie, range);
+
+        for (auto const& _creature : creaturedie)
+        {
+            auto loot = &_creature->loot;
+            gold += loot->gold;
+            loot->gold = 0;
+
+            uint8 lootSlot = 0;
+            uint8 maxSlot = loot->GetMaxSlotInLootFor(player);
+
+            for (uint32 i = 0; i < maxSlot; ++i)
+            {
+                if (LootItem* item = loot->LootItemInSlot(i, player))
+                {
+                    if (player->AddItem(item->itemid, item->count))
+                    {
+                        player->SendNotifyLootItemRemoved(lootSlot);
+                        player->SendLootRelease(player->GetLootGUID());
+                    }
+                    else
+                    {
+                        player->SendItemRetrievalMail(item->itemid, item->count);
+                        ChatHandler(player->GetSession()).SendSysMessage("Your items has been mailed to you.");
+                    }
+                }
+            }
+
+            loot->clear();
+
+            if (loot->isLooted() && loot->empty())
+            {
+                _creature->RemoveFlag(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_LOOTABLE);
+            }
+
+            loot->gold = gold;
+            player->ModifyMoney(loot->gold);
+            player->UpdateAchievementCriteria(ACHIEVEMENT_CRITERIA_TYPE_LOOT_MONEY, loot->gold);
+        }
+        return true;
+    }
+
+    void OnBeforeCreatureLootMoney(Player* player) override
+    {
+        bool _Enable = sConfigMgr->GetOption<bool>("AOELoot.Enable", true);
+
+        Creature* creature = nullptr;
+
+        if (player->GetGroup() || !creature || !_Enable)
+            return;
+
+        float range = 30.0f;
+        uint32 gold = 0;
+        Loot* loot = nullptr;
+        std::list<Creature*> creaturedie;
+        player->GetDeadCreatureListInGrid(creaturedie, range);
+
+        for (auto const& _creature : creaturedie)
+        {
+            loot = &_creature->loot;
+            gold += loot->gold;
+            loot->gold = 0;
+        }
+
+        loot->gold = gold;
     }
 };
 
-// Group all custom scripts
 void AddSC_AoeLoot()
 {
-    new AoeLoot_World();
+    new AoeLoot_Player();
 }
